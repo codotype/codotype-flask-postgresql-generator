2f6e672664e1557180ae32e2e784786a008290ef
Nitesh Dagli <ndagli16@gmail.com>
2018-10-25 15:12:34 -0700

    moved imports to the controller

--- generator/routes/templates/routes.py
+++ generator/routes/templates/routes.py
@@ -3,24 +3,7 @@ from app import api
 <%_ /*-----------------------------------------------*/ _%>
 <%_ /* """IMPORTS DEPENDENCIES - Iterates over each schema in blueprint""" */ _%>
 <%_ blueprint.schemas.forEach((schema) => { _%>
-<%_ /*-----------------------------------------------*/ _%>
-<%_ /*-----------------------------------------------*/ _%>
-from resources.<%= schema.identifier %> import <%= schema.class_name %>CollectionResource
-from resources.<%= schema.identifier %> import <%= schema.class_name %>ModelResource
-<%_ /*-----------------------------------------------*/ _%>
-<%_ /*-----------------------------------------------*/ _%>
-<%_ /* """Iterates over each schema in blueprint""" */ _%>
-<%_ schema.relations.forEach((rel) => { _%>
-<%_ /*-----------------------------------------------*/ _%>
-<%_ /*-----------------------------------------------*/ _%>
-<%_ if (rel.type === 'BELONGS_TO' || rel.type === "HAS_ONE") { _%>
-from resources.<%= schema.identifier %> import <%= schema.class_name %>Related<%= rel.alias.class_name %>Resource
-<%_ } else { _%>
-from resources.<%= schema.identifier %> import <%= schema.class_name %>Related<%= rel.alias.class_name_plural %>Resource
-<%_ } _%>
-<%_ /*-----------------------------------------------*/ _%>
-<%_ /*-----------------------------------------------*/ _%>
-<%_ }) _%>
+from resources.<%= schema.identifier %> import <%= makeResources(schema) %>
 <%_ }) _%>
 <%_ /* """ADDS ROUTES FOR DEPENDENCIES - Iterates over each schema in blueprint""" */ _%>
 <%_ /*-----------------------------------------------*/ _%>
